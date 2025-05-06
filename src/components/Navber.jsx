c9aa9a8145868e3e5f497304a560d8077374988a
arifurrahmansajid <iamnobody3456@gmail.com>
2025-05-06 12:30:57 +0600

    update navbar color and others

--- src/components/Navber.jsx
+++ src/components/Navber.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   const { user, logOut } = useContext(AuthContext);
 
   return (
-    <div className="bg-cyan-700">
+    <div className="bg-purple-500">
       <div className="navbar w-11/12 mx-auto">
         <div className="navbar-start">
           <div className="dropdown">
@@ -15,45 +15,45 @@ const Navbar = () => {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
               </svg>
             </div>
-            <ul tabIndex={0} className="menu menu-sm border-2 border-cyan-700 bg-cyan-700 bg-opacity-80 dropdown-content rounded-box z-[1] mt-3 w-52 p-2 shadow gap-2">
+            <ul tabIndex={0} className="menu menu-sm border-2 border-purple-700 bg-purple-700 bg-opacity-80 dropdown-content rounded-box z-[1] mt-3 w-52 p-2 shadow gap-2">
             {/* btn bg-cyan-400 hover:bg-cyan-500 hover: text-white border-none */}
-              <NavLink to="/" className="p-3 rounded-lg bg-cyan-400 hover:bg-cyan-500 hover: text-white border-none"> Home </NavLink>
-              <NavLink to="/program" className="p-3 rounded-lg bg-cyan-400 hover:bg-cyan-500 hover: text-white border-none"> Our Program </NavLink>
-              <NavLink to="/profile" className="p-3 rounded-lg bg-cyan-400 hover:bg-cyan-500 hover: text-white border-none"> My Profile </NavLink>
+              <NavLink to="/" className="p-3 rounded-lg bg-purple-400 hover:bg-purple-500 hover: text-white border-none"> Home </NavLink>
+              <NavLink to="/program" className="p-3 rounded-lg bg-purple-400 hover:bg-purple-500 hover: text-white border-none"> Our Program </NavLink>
+              <NavLink to="/profile" className="p-3 rounded-lg bg-purple-400 hover:bg-purple-500 hover: text-white border-none"> My Profile </NavLink>
             </ul>
           </div>
           <Link to="/" className="flex gap-2 btn btn-ghost text-xl">
             <img className="w-10" src="https://i.ibb.co.com/RTZDh9y/CC.png" alt="" />
-            <h2 className="text-xl font-bold text-white"> Career Counsellor </h2>
+            <h2 className="text-xl font-bold text-white">Event Explorer</h2>
           </Link>
         </div>
         <nav className="navbar-center gap-6 hidden lg:flex">
           <NavLink to="/" className="text-cyan-200 transition-colors font-bold"> Home </NavLink>
-          <NavLink to="/program" className="text-cyan-200 transition-colors font-bold"> Our Program </NavLink>
-          <NavLink to="/profile" className="text-cyan-300 transition-colors font-bold"> My Profile</NavLink>
+          <NavLink to="/program" className="text-purple-200 transition-colors font-bold"> Our Program </NavLink>
+          <NavLink to="/profile" className="text-purple-300 transition-colors font-bold"> My Profile</NavLink>
         </nav>
         <div className="navbar-end">
           {user ? (
             <div className="flex gap-2">
               <div className="flex items-center gap-4 group">
-                <button onClick={logOut} className="btn hidden md:flex bg-white hover:bg-cyan-100 text-cyan-700 font-bold border-none"> Logout </button>
+                <button onClick={logOut} className="btn hidden md:flex bg-white hover:bg-purple-100 text-purple-700 font-bold border-none"> Logout </button>
               </div>
               <div className="dropdown dropdown-end">
-                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar group-hover:bg-cyan-900">
+                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar group-hover:bg-purple-900">
                   <div className="w-10 rounded-full">
                     <img alt="Profile Avatar" src={user?.photoURL || "https://via.placeholder.com/150"}/>
                   </div>
                 </div>
-                <ul tabIndex={0} className="menu menu-sm border-2 border-cyan-700 bg-cyan-700 bg-opacity-90 gap-2 dropdown-content rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                <ul tabIndex={0} className="menu menu-sm border-2 border-purple-700 bg-purple-700 bg-opacity-90 gap-2 dropdown-content rounded-box z-[1] mt-3 w-52 p-2 shadow">
                   <div className="flex flex-col items-center justify-center">
                     <img className="w-16 h-16 rounded-full" src={user?.photoURL} alt="" />
                     <div className="py-3">
                       <h3 className="text-center font-bold pb-1 text-cyan-50">{user?.displayName}</h3>
                       <h3 className="text-center font-normal text-cyan-50">{user?.email}</h3>
                     </div>
-                    <Link to="/profile" className="btn bg-cyan-400 hover:bg-cyan-500 text-cyan-50 border-none w-28 mb-3"> My Profile </Link>
+                    <Link to="/profile" className="btn bg-purple-400 hover:bg-purple-500 text-cyan-50 border-none w-28 mb-3"> My Profile </Link>
                     {
-                      user ? <button onClick={logOut} className="btn bg-cyan-400 hover:bg-cyan-500 text-white border-none w-28"> Logout </button> : <Link to="/login" className="btn bg-cyan-400 hover:bg-cyan-500 hover: text-white border-none w-28"> Login </Link>
+                      user ? <button onClick={logOut} className="btn bg-purple-400 hover:bg-purple-500 text-white border-none w-28"> Logout </button> : <Link to="/login" className="btn bg-purple-400 hover:bg-purple-500 hover: text-white border-none w-28"> Login </Link>
                     }
                   </div>
                 </ul>
@@ -61,7 +61,7 @@ const Navbar = () => {
             </div>
           ) : (
             <div className="">
-              <Link to="/login" className="btn bg-cyan-300 hover:bg-cyan-600 text-white border-none"> Login </Link>
+              <Link to="/login" className="btn bg-purple-300 hover:bg-purple-600 text-white border-none"> Login </Link>
             </div>
           )}
         </div>
