013ec6309441262745c0ec522e1436673e4a4a96
arifurrahmansajid <iamnobody3456@gmail.com>
2025-05-06 12:52:34 +0600

    Add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,17 +2,13 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className='bg-[#0B3169] text-white'>
+    <div className='bg-purple-600 text-white'>
       <div className="flex flex-col md:flex-row gap-6 justify-between items-center w-11/12 mx-auto py-8 text-center md:text-left">
         <div className="flex flex-col gap-4">
-          <img className="w-36" src="https://i.ibb.co.com/RTZDh9y/CC.png" alt="" />
-          <Link to="/" className="text-xl font-bold"> Career Counsellor </Link>
+          <Link to="/" className="text-3xl font-bold"> Event Explorer </Link>
         </div>
         <div className="flex flex-col">
           <h2 className="text-lg font-bold mb-2"> Check Your Fitment </h2>
-          <p className="text-sm"> Premium Overseas Admission</p>
-          <p className="text-sm"> Student Profile Building </p>
-          <p className="text-sm"> Instructors </p>
           <p className="text-sm"> Pricing </p>
           <p className="text-sm"> Refund Policy </p>
           <p className="text-sm"> FAQs </p>
@@ -20,7 +16,6 @@ const Footer = () => {
         <div className="">
           <h2 className="text-lg font-bold mb-2"> Community </h2>
           <p className="text-sm"> Networking & Events </p>
-          <p className="text-sm"> Careers Transitioned </p>
           <p className="text-sm"> Achievements </p>
           <p className="text-sm"> Counselling Partners </p>
           <p className="text-sm"> Knowledge Sharing </p>
@@ -32,11 +27,10 @@ const Footer = () => {
           <p className="text-sm"> Meet The Team </p>
           <p className="text-sm"> Awards & Recognitions </p>
           <p className="text-sm"> Contact Us </p>
-          <p className="text-sm"> Pathway to excellence </p>
-          <p className="text-sm"> Set up Your Business </p>
+    
         </div>
       </div>
-      <p className="text-center text-sm p-4"> © Career Counselor 2024 </p>
+      <p className="text-center text-sm p-4"> © Event Explorer 2025 </p>
     </div>
   );
 };
